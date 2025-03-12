7547d3a220dbaf0f7405243b12e87301f95f9657
Ulises <armenterosu@gmail.com>
2025-03-12 17:01:04 -0300

    routes

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,4 @@
-import {Montserrat} from "next/font/google";
+import {Montserrat, Lusitana} from "next/font/google";
 
-export const montserrat = Montserrat({subsets: ["latin"]});
+export const montserrat = Montserrat({subsets: ["latin"], weight: ['400', '700']});
+export const lusitana = Lusitana({subsets: ["latin"], weight: ['400', '700']});
